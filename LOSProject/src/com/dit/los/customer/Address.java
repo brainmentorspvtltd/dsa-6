46399f23cb1db7e8dc77c24c9d7f42fc1458f0b8
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2021-06-12 12:11:16 +0530

    Add files via upload

---
+++ LOSProject/src/com/dit/los/customer/Address.java
@@ -0,0 +1,49 @@
+package com.dit.los.customer;
+
+public class Address {
+	
+	private String state;
+	private String city;
+	private String country;
+	private String pincode;
+	
+	public Address(String country, String city)
+	{
+		this.country = country;
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+	
+	
+}
---
+++ LOSProject/src/com/dit/los/utils/LoanConstants.java
@@ -0,0 +1,9 @@
+package com.dit.los.utils;
+
+public interface LoanConstants {
+	
+	String HOME_LOAN = "HL";
+	String AUTO_LOAN = "AL";
+	String PERSONAL_LOAN = "PL";
+
+}
