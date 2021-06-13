eb6e80ae23187afa40d8481036e2450865b6336d
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2021-06-13 11:53:03 +0530

    Add files via upload

---
+++ LOSProject-2/src/com/dit/los/utils/CommonConstants.java
@@ -0,0 +1,8 @@
+package com.dit.los.utils;
+
+public interface CommonConstants {
+	
+	char YES = 'Y';
+	char NO = 'N';
+
+}
---
+++ LOSProject-2/src/com/dit/los/utils/LoanConstants.java
@@ -0,0 +1,12 @@
+package com.dit.los.utils;
+
+public interface LoanConstants {
+	
+	String HOME_LOAN = "HL";
+	String AUTO_LOAN = "AL";
+	String PERSONAL_LOAN = "PL";
+	double HOME_LOAN_ROI = 0.9;
+	double AUTO_LOAN_ROI = 0.11;
+	double PERSONAL_LOAN_ROI = 0.14;
+
+}
